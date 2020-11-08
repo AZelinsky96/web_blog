80bc5a2daec729c391e589d8700f66d16c0bb6ba
Anthony <azelinsky1996@gmail.com>
2020-11-07 22:03:58 -0500

    Adding feature to create new posts

--- web_blog/app.py
+++ web_blog/app.py
@@ -1,16 +1,14 @@
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, make_response
 from web_blog.common.db_client_factory import initialize_database
 from web_blog.models.user import User
 from web_blog.models.blog import Blog
-
-import logging
+from web_blog.models.post import Post
 
 
 app = Flask(__name__)
 app.secret_key = "test_secret"
 mongo_db = initialize_database(connection_type="local", database_name="fullstack")
-logging.basicConfig(level=logging.DEBUG)
 
 
 def get_email_and_password():
@@ -63,6 +61,13 @@ def user_blogs(user_id=None):
     return render_template("user_blogs.html", blogs=blogs, email=user.email)
 
 
+@app.route("/posts/<string:blog_id>")
+def blog_posts(blog_id):
+    blog = Blog.get_blog_from_mongo(_id=blog_id, database=mongo_db)
+    posts = blog.find_posts_from_blog()
+    return render_template('posts.html', posts=posts, blog_title=blog.blog_title, blog_id=blog._id)
+
+
 @app.route("/blogs/new", methods=["POST", "GET"])
 def create_new_blog():
     if request.method == "GET":
@@ -70,7 +75,7 @@ def create_new_blog():
     else:
         title=request.form['title']
         description = request.form['description']
-        user = User.get_by_email(session['email'])
+        user = User.get_by_email(session['email'], mongo_db)
         
         new_blog = Blog(
             author=user.email,
@@ -83,11 +88,26 @@ def create_new_blog():
 
         return make_response(user_blogs(user._id))
 
-@app.route("/posts/<string:blog_id>")
-def blog_posts(blog_id):
-    blog = Blog.get_blog_from_mongo(_id=blog_id, database=mongo_db)
-    posts = blog.find_posts_from_blog()
-    return render_template('posts.html', posts=posts, blog_title=blog.blog_title, blog_id=blog._id)
+
+@app.route("/posts/new/<string:blog_id>", methods=["POST", "GET"])
+def create_new_post(blog_id):
+    if request.method == "GET":
+        return render_template("new_post.html", blog_id=blog_id)
+    else:
+        title=request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'], mongo_db)
+        
+        new_post = Post(
+            author=user.email,
+            title=title,
+            content=description,
+            blog_id=blog_id,
+            database=mongo_db
+        )
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))
 
 
 def main():
